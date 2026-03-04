655ce958f6cfeb986e35f194991671822945e145
Variel225 <varielaxcelinowhitejemus_2428250032@mhs.mdp.ac.id>
2026-03-04 10:46:08 +0700

    Home Screen & Detail Screen

---
+++ lib/models/movie.dart
@@ -0,0 +1,24 @@
+class Movie {
+  final int id;
+  final String title;
+  final String overview;
+  final String posterPath;
+  final String backdropPath;
+  final String releaseDate;
+  final double voteAvarage;
+
+  Movie({required this.id, required this.title, required this.overview, required this.posterPath, required this.backdropPath, required this.releaseDate, required this.voteAvarage});
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      id: json['id'] ?? '',
+      title: json['title'] ?? '', 
+      overview: json['overview'] ?? '', 
+      posterPath: json['poster_path'] ?? '', 
+      backdropPath: json['backdrop_path'] ?? '',
+      releaseDate: json['release_date'] ?? '',
+      voteAvarage: json['vote_average'].toDouble() ?? 0.0,
+    );
+  }
+}
+
