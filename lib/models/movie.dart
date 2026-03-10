f177f140000475e8e068b3dee7927b3e1007d38a
Variel225 <varielaxcelinowhitejemus_2428250032@mhs.mdp.ac.id>
2026-03-10 09:27:40 +0700

    FavoriteScreen & SearchScreen

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -5,9 +5,9 @@ class Movie {
   final String posterPath;
   final String backdropPath;
   final String releaseDate;
-  final double voteAvarage;
+  final double voteAverage;
 
-  Movie({required this.id, required this.title, required this.overview, required this.posterPath, required this.backdropPath, required this.releaseDate, required this.voteAvarage});
+  Movie({required this.id, required this.title, required this.overview, required this.posterPath, required this.backdropPath, required this.releaseDate, required this.voteAverage});
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
@@ -17,8 +17,19 @@ class Movie {
       posterPath: json['poster_path'] ?? '', 
       backdropPath: json['backdrop_path'] ?? '',
       releaseDate: json['release_date'] ?? '',
-      voteAvarage: json['vote_average'].toDouble() ?? 0.0,
+      voteAverage: json['vote_average'].toDouble() ?? 0.0,
     );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'overview': overview,
+      'poster_path': posterPath,
+      'backdrop_path': backdropPath,
+      'release_date': releaseDate,
+      'vote_average': voteAverage,
+    };
   }
 }
 
